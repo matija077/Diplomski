81e272981401c53e3f7814c276ce04f32edde58b
Matija077 <matija.prs@gmail.com>
2020-07-11 15:34:27 +0200

    identity working

--- index.js
+++ index.js
@@ -6,19 +6,39 @@ var Dash = require('Dash');
 const clientOps = {
     network: 'testnet',
     wallet: {
-        mnemonic: null,
+        mnemonic: 'waste hawk artist palm label govern slim camp among soda mimic whale',
     },
 };
+//ybePQfwd636NAYzMrHxsk4HZLS74aKPTjJ
 
 const clientOps2 = {
     network: 'testnet',
     wallet: {
-        mnemonic: null,
+        mnemonic: 'net mechanic visual various skin remove stool merit dog invite ready shoulder',
     },
 };
+// ybKetMrbXDBKLrfqA4iLf48bR5CfFrmw1m
 
-var client = new Dash.Client(clientOps);
+// use this to get dash http://faucet.evonet.networks.dash.org/
+
+/*var client = new Dash.Client(clientOps);
 var client2 = new Dash.Client(clientOps2);
+
+setFirstTime(client,clientOps);
+setFirstTime(client2,clientOps2);*/
+
+client = new Dash.Client(clientOps);
+client2 = new Dash.Client(clientOps2);
+
+async function setFirstTime(client, clientOps) {
+    try {
+        clientOps.wallet.mnemonic = await client.wallet.exportWallet();
+
+        client.disconnect();
+    } catch(error) {
+        console.log(error);
+    }
+}
 
 async function connect(client) {
     var actualClient;
@@ -46,47 +66,97 @@ async function createWallet(client) {
     return wallet;
 }
 
-
-async function createTransaction(wallet) {
+async function createTransaction(account, address, money) {
     var result;
 
     try {
-        const transaction = wallet.createTransaction({
-            recepient: 'yNPbcFfabtNmmxKdGwhHomdYfVs6gikbPf',
-            satoshis: 100000000,
+        const transaction = account.createTransaction({
+            recipient: address,
+            satoshis: money,
         });
 
-        result = await wallet.broadcastTransaction(transaction);
+        //account.sign(transaction);
+
+        result = await account.broadcastTransaction(transaction);
+        return result;
     }  catch(error) {
         throw error;
     }
-
-    return result;
 }
+
+async function createIdentity(client) {
+    var identity;
+    //console.log(client);
+
+    try {
+        const platform = client.platform;
+        //console.log(platform);
+        identity = await platform.identities.register();
+        //console.log({identity});
+  } catch (e) {
+        console.error('Something went wrong:', e);
+  } finally {
+        //client.disconnect();
+  }
+
+  return identity;
+}
+
+var first = {};
+var second = {};
 
 Promise.all([createOneWallet(client), createOneWallet(client2)])
     .then(function resolved(results) {
-        console.log(results);
-        var {dapiClient1, wallet1} = results[0];
-        var {dapiClient2, wallet2} = results[1];
+        //console.log(results);
+       /* var {dapiClient1, wallet1} = results[0];
+        var {dapiClient2, wallet2} = results[1];*/
 
-        var first = {
+        /*first = {
             client: client,
             dapiClient: dapiClient1,
             mnemonic: wallet1.mnemonic,
             account: wallet1.account,
             address: wallet1.address,
-        };
-        var second = {
+        };*/
+        first = {client, ...results[0]};
+        /*second = {
             client: client,
             dapiClient: dapiClient2,
             mnemonic: wallet2.mnemonic,
             account: wallet2.account,
             address: wallet2.address,
-        };
+        };*/
+        second = {...results[1]};
+        second.client = client2
+        //console.log(first);
 
-        console.log(first);
+        console.log(first.account.getTotalBalance());
+        console.log(second.account.getTotalBalance());
 
+        let transaction;
+        /*createTransaction(first.account, second.address.address, 100000000).then(
+            function transactionBroadcasted(result) {
+                transaction = result;
+                console.log(transaction);
+        });*/
+        /*console.log(first);
+        console.log(second);*/
+
+        createIdentity(first.client).then(
+            function(result) {
+                first.identity = result;
+                console.log(first.identity);
+            }
+        );
+        createIdentity(second.client).then(
+            function(result) {
+                second.identity = result;
+                console.log(second.identity);
+            }
+        );
+
+        console.log(first.account.getTotalBalance());
+        console.log(second.account.getTotalBalance());
     }).catch(function rejected(reason) {
         console.log(reason);
     }
