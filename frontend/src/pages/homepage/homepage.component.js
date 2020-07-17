7ed1b03a5141b5c8f10c6f3c19747bd9c0b820e6
Matija077 <matija.prs@gmail.com>
2020-07-17 13:10:45 +0200

    add header and homepage
    add header component
    add homepage component

---
+++ frontend/src/pages/homepage/homepage.component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.style.css';
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            <div className="options">
+                <span className="option">Projects</span>
+                <span className="option">My Projects</span>
+            </div>
+        </div>
+    );
+}
+
+export  default HomePage;
