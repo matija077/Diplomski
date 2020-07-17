51df196d4206e0b1d92a02fefa4927c259b6aae1
Matija077 <matija.prs@gmail.com>
2020-07-17 15:17:55 +0200

    add auth and projects pages
    add auth page with routing for existign user
    add header change depending on the user
    add projects page and routing
    clean app.css and add font

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,33 @@
 import React from 'react';
 
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
-
+import Projects from './pages/projects/projects.component';
+import Authentication from './pages/authentication/authentication.component';
 import './App.css';
 
 function App() {
+  var user = null;
+
   return (
     <div>
       <Header></Header>
       <Switch>
         <Route exact component={HomePage} path='/'></Route>
         <Route component={null} path='/projects:1'></Route>
-        <Route component={null} path='/projects'></Route>
+        <Route component={Projects} path='/projects'></Route>
         <Route component={null} path='my_projects'></Route>
+        <Route
+          render={() =>
+            user?
+              (<Redirect to="/"></Redirect>)
+            :
+              (<Authentication></Authentication>)
+          }
+          path='/signin'
+        ></Route>
       </Switch>
     </div>
   );
---
+++ frontend/src/pages/projects/projects.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './projects.style.scss';
+
+class Projects extends React.Component {
+    constructor () {
+        super();
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+
+        return (
+            <h1>
+                Projects
+            </h1>
+        );
+    }
+}
+
+export default Projects;
