7bf762397369c36fb9b5d471fd747e7454ec0c42
Matija077 <matija.prs@gmail.com>
2020-08-08 18:50:11 +0200

    workign replace, delete . contract signage
    contract signage needs testing

--- src/Project/queryOptions.js
+++ src/Project/queryOptions.js
@@ -1,9 +1,12 @@
-function queryOptionsFindById(id, limit = 0) {
+function queryOptionsFindById(
+    id, limit = 0, startAt = undefined, startAfter) {
     var queryOptions = {
         where: [
             ['$ownerId', '==', id],
         ],
         limit: limit,
+        startAt: startAt,
+        startAfter: startAfter
     }
 
     return queryOptions;
--- src/createDeleteBatch.js
+++ src/createDeleteBatch.js
@@ -1,13 +1,13 @@
 var DocumentBatch = require('./documentBatch');
 
-function createReplaceBatch(documents) {
+function createDocumentBatch(documents) {
     if (!Array.isArray(documents)) {
         documents = [documents];
     }
 
-    var documentBatch = new DocumentBatch([], documents);
+    var documentBatch = new DocumentBatch([], [], documents);
 
     return documentBatch;
 }
 
-module.exports = createReplaceBatch;
+module.exports = createDocumentBatch;
